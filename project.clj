e77b59b1232e8b2a8917a220a81bad81c582de85
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-08-28 18:42:18 +0300

    Make project work with 'lein repl'! yay :D
    (require 'beatstream.main)
    (beatstream.main/-main)

--- project.clj
+++ project.clj
@@ -4,18 +4,17 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [ring/ring-json "0.2.0"]
+                 [lib-noir "0.6.4"]
+                 [ring/ring-jetty-adapter "1.1.0"]
                  [com.h2database/h2 "1.3.170"]
                  [lobos "1.0.0-beta1"]
                  [korma "0.3.0-RC5"]
                  [sonian/carica "1.0.3"]
-                 [lib-noir "0.6.4"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
+                 [org/jaudiotagger "2.0.3"]]
                  ; [liberator "0.9.0"]
-  :plugins [[lein-ring "0.8.5"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [ring-mock "0.1.5"]]}
              :production {:ring {:open-browser? false
                                  :auto-reload? false
                                  :stacktraces? false}}}
-  :ring {:handler beatstream.server/app}
   :main beatstream.main)
--- src/beatstream/main.clj
+++ src/beatstream/main.clj
@@ -1,9 +1,47 @@
 (ns beatstream.main
-  (:require [beatstream.server :as server])
-  (:gen-class))
+  (:use compojure.core)
+  (:gen-class)
+  (:require [beatstream.controllers.common :as common]
+            [beatstream.controllers.profile :as profile]
+            [beatstream.controllers.songs :as songs]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.file :refer [wrap-file]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [ring.util.response :as resp]
+            [noir.util.middleware :refer [wrap-strip-trailing-slash wrap-canonical-host wrap-force-ssl]]))
 
-(defn start-server []
-  (run-jetty (server/app) {:port 8080 :join? false}))
+(defroutes api-routes
+  (context "/profile" []
+    (defroutes profile-routes
+      (GET "/" [] (profile/show))
+      (GET "/lastfm" [] (profile/lastfm))
+      (PUT "/" {body :body} (profile/update body))))
+  (context "/songs" []
+    (defroutes songs-routes
+      (GET "/" [] (songs/all))
+      (GET "/play" {params :params} (songs/play params))
+      (POST "/" [] (songs/refresh))))
+  (PUT "now-playing" [] "update now playing")
+  (POST "scrobble" [] "scrobble the now playing OR specified song")
+  (route/not-found (common/not-found)))
 
-(defn -main [& args]
-  (start-server))
+(defroutes public-routes
+  (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
+  (route/resources "/")
+  (route/not-found (common/not-found)))
+
+(def app
+  (routes
+    (handler/api
+      (context "/api/v1" []
+        (-> api-routes
+            (wrap-json-body)
+            (wrap-json-response))))
+    (handler/site public-routes)))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port 5000))]
+    (jetty/run-jetty app {:port port :join? false})))
