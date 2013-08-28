19a98e4cac80f47d760a4e4779fcd23f245b8738
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-08-28 18:09:24 +0300

    Find song files (currently MP3s)

--- src/beatstream/controllers/songs.clj
+++ src/beatstream/controllers/songs.clj
@@ -1,13 +1,27 @@
 (ns beatstream.controllers.songs
-  (:use ring.util.response))
+  (:use ring.util.response)
+  (:require clojure.java.io))
+
+(def music-path "/Users/ajk/Music/")
+
+(defn get-song-files [directory]
+  (->> directory
+       clojure.java.io/file
+       file-seq
+       (filter #(.isFile %))
+       (map #(.getAbsolutePath %))
+       (filter #(re-find #"(?i)\.mp3$" %))))
 
 (defn update-medialibrary []
-  (str "get music_path, look for files with certain extension, read ID3, add file to database with ID3 info"))
+  (str "TODO: read ID3, add file to database with ID3 info")
+  (get-song-files music-path))
 
 (defn all []
+  "Return all the media files currently in the DB"
   (response []))
 
 (defn refresh []
+  "Find all the media files and add them + their meta data into DB"
   (update-medialibrary))
 
 (defn play [params]
