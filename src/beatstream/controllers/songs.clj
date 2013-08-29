071d685a81210717a8b486ff1e8f949049ee6612
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-08-29 08:50:09 +0300

    Add first dabbles at using jaudiotagger

--- src/beatstream/controllers/songs.clj
+++ src/beatstream/controllers/songs.clj
@@ -1,6 +1,8 @@
 (ns beatstream.controllers.songs
   (:use ring.util.response)
-  (:require clojure.java.io))
+  (:require clojure.java.io)
+  (:import [org.jaudiotagger.audio AudioFileIO]
+           [org.jaudiotagger.tag FieldKey]))
 
 (def music-path "/Users/ajk/Music/")
 
@@ -9,16 +11,18 @@
        clojure.java.io/file
        file-seq
        (filter #(.isFile %))
-       (map #(.getAbsolutePath %))
-       (filter #(re-find #"(?i)\.mp3$" %))))
+       (filter #(re-find #"(?i)\.(mp3|ogg)$" (.getAbsolutePath %)))))
 
 (defn update-medialibrary []
-  (str "TODO: read ID3, add file to database with ID3 info")
-  (get-song-files music-path))
+  "TODO: read ID3, add file to database with ID3 info"
+  "artist, album, title, album artist, tracknumber, title, duration, year, genre, file path"
+  (doseq [f (get-song-files music-path)]
+    (println (.getFirst (.getTag (AudioFileIO/read f)) FieldKey/ARTIST))))
 
 (defn all []
   "Return all the media files currently in the DB"
-  (response []))
+  (update-medialibrary))
+  ; (response []))
 
 (defn refresh []
   "Find all the media files and add them + their meta data into DB"
